785c7fe13ef53073034fd7b27f3fa85f50094126
John Kelley <jkelley@investvoyager.com>
2022-07-24 01:14:06 -0400

    first commit

---
+++ data/students/studentData.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "studentOne": {
+            "firstName": "john",
+            "id": 1,
+            "lastName": "kelley",
+            "nationality": "American",
+            "studentClass": "Math 101"
+    },
+    "studentTwo": {
+            "firstName": "Bob",
+            "id": 2,
+            "lastName": "Barker",
+            "nationality": "American",
+            "studentClass": "The Price is Right 101"
+    }
+}
